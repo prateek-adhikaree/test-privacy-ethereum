61c0de1758b118977941ceef6e348e01a0c3a6e9
prateek <prateek.adhikaree@blocktest.net>
2018-11-29 18:08:56 -0500

    initial commit

---
+++ contracts/TestTestPrivacy.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.24;
+
+import "./TestPrivacy.sol";
+
+contract TestTestPrivacy is TestPrivacy {
+    
+    // only user added in the system can do this
+    function testIsUserInAllowedList(){
+        require (isUserInAllowedList(), "owner not in allowed list"); // returns (bool isUserAllowed)
+    }
+    
+    // only owner
+    function testAddNewUser(){
+        address newUser = 0xdd870fa1b7c4700f2bd7f44238821c26f7392148;
+        addNewUser(newUser);
+        require(allowedUsers[newUser], "User not added to the map");
+    }
+    
+    // only user added in the system can do this
+    function testShowHiddenValue(){
+        showHiddenValue(); // returns (string _hiddenValue)
+    }
+    
+    // only owner
+    function testChangeHiddenValue(){
+        string memory prevValue = showHiddenValue();
+        string memory newValue = "testing";
+        changeHiddenValue(newValue);
+        require(keccak256(showHiddenValue()) == keccak256(newValue), "Value not changed");
+    }
+}
