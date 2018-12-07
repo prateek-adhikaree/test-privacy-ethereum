cac0c17f652e9aea3c8cad8c197513b3266e222c
prateek <prateek.adhikaree@blocktest.net>
2018-12-07 09:54:02 -0500

    updating code for v0.4.24

--- contracts/TestTestPrivacy.sol
+++ contracts/TestTestPrivacy.sol
@@ -1,31 +1,29 @@
-pragma solidity ^0.5.0;
-
-import "./TestPrivacy.sol";
-
-contract TestTestPrivacy is TestPrivacy {
+pragma solidity ^0.4.24;
+ import "./TestPrivacy.sol";
+ contract TestTestPrivacy is TestPrivacy {
     
     // only user added in the system can do this
-    function testIsUserInAllowedList() public {
+    function testIsUserInAllowedList(){
         require (isUserInAllowedList(), "owner not in allowed list"); // returns (bool isUserAllowed)
     }
     
     // only owner
-    function testAddNewUser() public {
-        address newUser = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+    function testAddNewUser(){
+        address newUser = 0xdd870fa1b7c4700f2bd7f44238821c26f7392148;
         addNewUser(newUser);
         require(allowedUsers[newUser], "User not added to the map");
     }
     
     // only user added in the system can do this
-    function testShowHiddenValue() public {
+    function testShowHiddenValue(){
         showHiddenValue(); // returns (string _hiddenValue)
     }
     
     // only owner
-    function testChangeHiddenValue() public {
+    function testChangeHiddenValue(){
         string memory prevValue = showHiddenValue();
         string memory newValue = "testing";
         changeHiddenValue(newValue);
-        require(keccak256(bytes(prevValue)) == keccak256(bytes(newValue)), "Value not changed");
+        require(keccak256(showHiddenValue()) == keccak256(newValue), "Value not changed");
     }
-}
+} 
